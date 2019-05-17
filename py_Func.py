0d22fbe37c719d6e7e0c5e66feb604fea65aad83
azfarbkhr <azfarbkhr@gmail.com>
2019-05-18 00:36:56 +0500

    minor changes

--- py_Func.py
+++ py_Func.py
@@ -3,13 +3,38 @@ REQUIREMENTS
 3.	Write a Python function to check whether a number is completely divisible by another number. Accept two integer values form the user
 '''
 
-print("Requirements of the program")
+print("Requirements of the program:")
 print("Write a Python function to check whether a number is completely divisible by another number. Accept two integer values form the user.")
 
-while True:
+inputValues = {"number1": 0, "number2": 0}
+
+
+for i in inputValues:
+    while True:
+        try:
+            inputValues[i] = int(input("Please enter a number: "))
+            if inputValues[i] == 0:
+                raise ZeroDivisionError
+            break
+        except ZeroDivisionError:
+            print("Error 02: You have entered second number as zero and nothing is divisible by zero.")
+        except:
+            print("Error 01: You have entered an invalid number.")
+
+
+
+def isDivisible(a, b):
     try:
-        number1 = int(input("Please enter a value for numerator: "))
-        number2 = int(input("Please enter a value for denominator: "))
-        break
-    except ValueError:
-        print("Error 01: You have entered an invalid number.")
+        if inputValues["number1"] % inputValues["number2"] > 0:
+            return False
+        else:
+            return True
+    except ZeroDivisionError:
+        return False
+
+
+if isDivisible(inputValues["number1"], inputValues["number2"]):
+    print("The first number is completely divisible by the second number.")
+else:
+    print("The first number is not completely divisible by the second number.")
+
