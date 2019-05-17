d2d5822f8795ad4cf7a225af660f0183c84dfd7c
azfarbkhr <azfarbkhr@gmail.com>
2019-05-17 06:34:58 +0500

    area of circle assignment completed

--- area_circle.py
+++ area_circle.py
@@ -6,7 +6,15 @@ REQUIREMENTS
 while True:
     try:
         radius = int(input("Please enter radius of circle: "))
+        if radius <= 0:
+            raise ZeroDivisionError
         break
-    except:
-        print("Error 01: You have entered an incorrect number.")
-    
+    except ValueError:
+        print("Error 01: You have entered an invalid number.")
+    except ZeroDivisionError:
+        print("Error 02: You have entered a number less than or equal to zero.")
+
+
+cricleArea = int((22 / 7) * (radius ** 2))
+
+print("The area of circle is {x}".format(x = cricleArea))
