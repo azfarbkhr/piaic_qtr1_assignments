4758184d5a0c6086ddd14f5afb28af83138b8364
azfarbkhr <azfarbkhr@gmail.com>
2019-05-19 02:39:58 +0500

    assignment completed for computing expression n + nn + nnn

--- n_to_nnn.py
+++ n_to_nnn.py
@@ -3,14 +3,22 @@ REQUIREMENTS
 4.	Write a Python program that accepts an integer (n) and computes the value of (n + nn + nnn)
 '''
 
-# list of variables
+
+# list of variables to be used
 number = 0
+sumOfNumber = 0
+tempNumber = str("")
+sumExpress = str("")
+
+# print requirements
+print("Requirements: ")
+print("a Python program that accepts an integer (n) and computes the value of (n + nn + nnn)")
 
 
 # accept input from user and validate it
 while True:
     try:
-        number = int(input("Please enter radius of circle: "))
+        number = int(input("Please enter a number (n): "))
         if number <= 0:
             raise ZeroDivisionError
         break
@@ -20,6 +28,12 @@ while True:
         print("Error 02: You have entered a number less than or equal to zero.")
 
 
-# print n times a number n 
-for n in range(number):
-    print(n)
+# print n times a number n
+for count in range(1, number+1):
+    tempNumber = ""
+    for i in range(count):
+        tempNumber = int(str(tempNumber) + str(number))
+    sumExpress = sumExpress + " + " + str(tempNumber)
+    sumOfNumber = sumOfNumber + int(tempNumber)
+
+print("the value of expression ({x} ) computes to {y}.".format(x = sumExpress, y = sumOfNumber))
