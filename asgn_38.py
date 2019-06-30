b0bca4fcf9e4a42d9199ebb2eab3641e7604efca
azfarbkhr <azfarbkhr@gmail.com>
2019-06-30 13:22:47 +0500

    all assignments completed

--- asgn_38.py
+++ asgn_38.py
@@ -3,3 +3,12 @@ Requirement
 38.	Write a Python program to get the Fibonacci series between 0 to 50
 '''
 
+# variables used
+x = 0
+y = 1
+
+# logic used
+
+while y < 50:
+    print(y)
+    x,y = y,x+y
